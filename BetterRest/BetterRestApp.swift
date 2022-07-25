8688443d7fbb5da49145e4ca93cb3a5de6a7a28f
Jiaming Guo <j23guo@gmail.com>
2022-07-25 19:24:38 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jiaming Guo on 2022-07-25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
