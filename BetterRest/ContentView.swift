bc2cd33742e0168da22aa658f684321f4db96484
Jiaming Guo <j23guo@gmail.com>
2022-07-25 20:26:34 -0400

    connecting core ml

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -6,14 +6,32 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     func calculateBedTime() {
-        
+        do {
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal sleep time is ..."
+            alertMessage = "\(sleepTime.formatted(date: .omitted, time: .shortened))"
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
     }
     
     var body: some View {
@@ -21,7 +39,7 @@ struct ContentView: View {
             VStack {
                 Text("When do you want to wake up?")
                     .font(.headline)
-                DatePicker("Please enter a time", selection: $wakeUp, in: Date.now..., displayedComponents: .hourAndMinute)
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                     .labelsHidden()
                 Text("Desired amount of sleep")
                     .font(.headline)
@@ -37,6 +55,11 @@ struct ContentView: View {
             .navigationTitle("BetterRest")
             .toolbar {
                 Button("Calculate", action: calculateBedTime)
+            }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("OK") { }
+            } message: {
+                Text(alertMessage)
             }
         }
     }
